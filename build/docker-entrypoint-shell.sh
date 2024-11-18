5182cb54f4c6060bea6c06ba9009475327958e2c
undefined <undefined>
2024-11-18 22:49:31 +0100

    add function log + add_if_not_exists

--- build/docker-entrypoint-shell.sh
+++ build/docker-entrypoint-shell.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 set -e
 
+# Function to display colored logs and write to the log file
+log() {
+    local level="$1"
+    shift
+    local message="$@"
+    local log_message
+
+    case "$level" in
+        "INFO") log_message="${GREEN}[INFO] ${message}${RESET}" ;;
+        "WARNING") log_message="${YELLOW}[WARNING] ${message}${RESET}" ;;
+        "ERROR") log_message="${RED}[ERROR] ${message}${RESET}" ;;
+        "DEBUG") log_message="${BLUE}[DEBUG] ${message}${RESET}" ;;
+        *) log_message="[UNKNOWN] $message" ;;
+    esac
+
+    # Output to console and log file
+    echo -e "$log_message"
+    #echo "[$(date '+%Y-%m-%d %H:%M:%S')] $level: $message" >> "$LOG_FILE"
+}
+
+add_if_not_exists() {
+    local file="$1"
+    local keyword="$2"
+    local line="$3"
+    
+    if ! grep -q "^$keyword:" "$file"; then
+        echo "$line" >> "$file"
+        log "INFO" "Added '$line' to $file"
+    else
+        log "INFO" "'$keyword:' already exists in $file. Skipping addition."
+    fi
+}
+
 # Exécution en fonction du type de composant Salt
 if [ "$SALT_NODE_TYPE" = "MASTER" ]; then
     echo "Starting Salt Master..."
     exec salt-master -l debug
 elif [ "$SALT_NODE_TYPE" = "SYNDIC" ]; then
-    #sed -i "s/id: .*/id: $SALT_HOSTNAME/g" >> /etc/salt/minion
-    echo "id: $SALT_HOSTNAME" >> /etc/salt/minion
+    add_if_not_exists /etc/salt/minion.d/minion.conf "id:" "id: $SALT_HOSTNAME"
     echo "Starting Salt Syndic..."
     exec salt-master -l debug
     exec salt-syndic -l debug
     exec salt-minion -l debug
 elif [ "$SALT_NODE_TYPE" = "MINION" ]; then
-    #sed -i "s/id: .*/id: $(hostname)/g" >> /etc/salt/minion
-    echo "id: $(hostname)" >> /etc/salt/minion
+    add_if_not_exists /etc/salt/minion.d/minion.conf "id:" "id: $(hostname)"
     echo "Starting Salt Minion..."
     exec salt-minion -l debug
 else
--- build/docker-entrypoint-supervisor.sh
+++ build/docker-entrypoint-supervisor.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
 set -e
 
+# Function to display colored logs and write to the log file
+log() {
+    local level="$1"
+    shift
+    local message="$@"
+    local log_message
+
+    case "$level" in
+        "INFO") log_message="${GREEN}[INFO] ${message}${RESET}" ;;
+        "WARNING") log_message="${YELLOW}[WARNING] ${message}${RESET}" ;;
+        "ERROR") log_message="${RED}[ERROR] ${message}${RESET}" ;;
+        "DEBUG") log_message="${BLUE}[DEBUG] ${message}${RESET}" ;;
+        *) log_message="[UNKNOWN] $message" ;;
+    esac
+
+    # Output to console and log file
+    echo -e "$log_message"
+    #echo "[$(date '+%Y-%m-%d %H:%M:%S')] $level: $message" >> "$LOG_FILE"
+}
+
+add_if_not_exists() {
+    local file="$1"
+    local keyword="$2"
+    local line="$3"
+    
+    if ! grep -q "^$keyword:" "$file"; then
+        echo "$line" >> "$file"
+        log "INFO" "Added '$line' to $file"
+    else
+        log "INFO" "'$keyword:' already exists in $file. Skipping addition."
+    fi
+}
+
 # Exécution en fonction du type de serveur Salt
 if [ "$SALT_NODE_TYPE" = "MASTER" ]; then
     echo "Salt Master configuration preprocessing..."
 elif [ "$SALT_NODE_TYPE" = "SYNDIC" ]; then
     echo "Salt Syndic configuration preprocessing..."
-    echo "id: $SALT_HOSTNAME" >> /etc/salt/master.d/master.conf
-    echo "id: $SALT_HOSTNAME" >> /etc/salt/minion.d/minion.conf
+    add_if_not_exists /etc/salt/master.d/master.conf "id:" "id: $SALT_HOSTNAME"
+    add_if_not_exists /etc/salt/minion.d/minion.conf "id:" "id: $SALT_HOSTNAME"
 elif [ "$SALT_NODE_TYPE" = "MINION" ]; then
     echo "Salt Minion configuration preprocessing..."
-    echo "id: $SALT_HOSTNAME" >> /etc/salt//minion.d/minion.conf
+    add_if_not_exists /etc/salt/minion.d/minion.conf "id:" "id: $SALT_HOSTNAME"
+    add_if_not_exists /etc/salt/minion.d/minion.conf "auth_timeout:" "auth_timeout: 5" # to fix : salt.exceptions.SaltClientError: Unable to sign_in to master: Attempt to authenticate with the salt master failed with timeout error
 else
     echo "Runtime Error: Invalid SALT_NODE_TYPE. Must be either MASTER, SYNDIC or MINION."
     exit 1
